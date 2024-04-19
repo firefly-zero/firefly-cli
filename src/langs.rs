3c1d03ee7a6133c2b639ec74aa0dc3c29a92f6b0
gram <git@orsinium.dev>
2024-04-19 14:12:02 +0200

    build rust examples

--- src/langs.rs
+++ src/langs.rs
@@ -5,7 +5,7 @@ use std::env::temp_dir;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Output};
 
 pub(crate) fn build_bin(config: &Config) -> anyhow::Result<()> {
     let lang: Lang = match &config.lang {
@@ -45,6 +45,7 @@ fn detect_lang(root: &Path) -> anyhow::Result<Lang> {
     bail!("failed to detect the programming language");
 }
 
+/// Build Go code using TinyGo.
 fn build_go(config: &Config) -> anyhow::Result<()> {
     let target_path = find_tinygo_target(config)?;
     let target_path = path_to_utf8(&target_path)?;
@@ -56,13 +57,7 @@ fn build_go(config: &Config) -> anyhow::Result<()> {
         .current_dir(in_path)
         .output()
         .context("run tinygo build")?;
-    std::io::stdout().write_all(&output.stdout)?;
-    std::io::stderr().write_all(&output.stderr)?;
-    if !output.status.success() {
-        let code = output.status.code().unwrap_or(1);
-        bail!("subprocess exited with status code {code}");
-    }
-    Ok(())
+    check_output(output)
 }
 
 /// Get the path to target.json in the project root or create a temporary one.
@@ -80,7 +75,51 @@ fn find_tinygo_target(config: &Config) -> anyhow::Result<PathBuf> {
     Ok(target_path)
 }
 
-fn build_rust(_config: &Config) -> anyhow::Result<()> {
+fn build_rust(config: &Config) -> anyhow::Result<()> {
+    if config.root_path.join("Cargo.toml").exists() {
+        build_rust_project(config)
+    } else {
+        build_rust_example(config)
+    }
+}
+
+/// Build rust code example (must be a directory).
+///
+/// http://xion.io/post/code/rust-examples.html
+fn build_rust_example(config: &Config) -> anyhow::Result<()> {
+    let example_name = match config.root_path.file_name() {
+        Some(dir_name) => dir_name,
+        None => bail!("empty project path"),
+    };
+    let Some(example_name) = example_name.to_str() else {
+        bail!("cannot convert project directory name to UTF-8")
+    };
+    let cargo_out_dir = temp_dir();
+    let in_path = path_to_utf8(&config.root_path)?;
+    let cmd_args = [
+        "build",
+        "--target",
+        "wasm32-unknown-unknown",
+        "--out-dir",
+        path_to_utf8(&cargo_out_dir)?,
+        "-Z",
+        "unstable-options",
+        "--example",
+        example_name,
+    ];
+    let output = Command::new("cargo")
+        .args(cmd_args)
+        .current_dir(in_path)
+        .output()
+        .context("run cargo build")?;
+    check_output(output)?;
+    let cargo_out_path = cargo_out_dir.join(format!("{example_name}.wasm"));
+    let out_path = config.rom_path.join("bin");
+    std::fs::copy(cargo_out_path, out_path)?;
+    Ok(())
+}
+
+fn build_rust_project(_config: &Config) -> anyhow::Result<()> {
     todo!()
 }
 
@@ -98,4 +137,14 @@ fn path_to_utf8(path: &Path) -> anyhow::Result<&str> {
         Some(path) => Ok(path),
         None => bail!("project root path cannot be converted to UTF-8"),
     }
+}
+
+fn check_output(output: Output) -> anyhow::Result<()> {
+    std::io::stdout().write_all(&output.stdout)?;
+    std::io::stderr().write_all(&output.stderr)?;
+    if !output.status.success() {
+        let code = output.status.code().unwrap_or(1);
+        bail!("subprocess exited with status code {code}");
+    }
+    Ok(())
 }
