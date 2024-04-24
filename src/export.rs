215b621d4e4adf971fc6a550889a97353dccf901
gram <git@orsinium.dev>
2024-04-24 15:28:31 +0200

    fox export, simplify import

--- src/export.rs
+++ src/export.rs
@@ -2,7 +2,7 @@ use crate::args::ExportArgs;
 use crate::config::Config;
 use crate::vfs::get_vfs_path;
 use anyhow::{Context, Result};
-use std::fs;
+use std::fs::{read_dir, File};
 use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use zip::write::FileOptions;
@@ -37,15 +37,14 @@ fn get_id(args: &ExportArgs) -> Result<(String, String)> {
 fn archive(in_path: &Path, out_path: &Path) -> Result<()> {
     // Should go first so that we don't create empty archive
     // if ROM doesn't exist.
-    let entries = fs::read_dir(in_path).context("read ROM dir")?;
+    let entries = read_dir(in_path).context("read ROM dir")?;
 
-    let out_file = fs::File::create(out_path).context("create archive file")?;
+    let out_file = File::create(out_path).context("create archive file")?;
     let mut zip = ZipWriter::new(out_file);
     let options = FileOptions::<()>::default()
         .compression_method(CompressionMethod::Zstd)
         .unix_permissions(0o755);
 
-    let mut buffer = Vec::new();
     for entry in entries {
         let entry = entry.context("get dir entry")?;
         let file_path = entry.file_name();
@@ -54,8 +53,9 @@ fn archive(in_path: &Path, out_path: &Path) -> Result<()> {
         zip.start_file(file_path, options)
             .context("create file in archive")?;
         let path = entry.path();
-        let mut f = fs::File::open(path).context("open file in ROM")?;
-        f.read_to_end(&mut buffer).context("read file")?;
+        let mut file = File::open(path).context("open file in ROM")?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).context("read file")?;
         zip.write_all(&buffer).context("write file into archive")?;
     }
     Ok(())
--- src/import.rs
+++ src/import.rs
@@ -1,6 +1,7 @@
 use crate::args::ImportArgs;
 use crate::vfs::{get_vfs_path, init_vfs};
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
+use firefly_meta::Meta;
 use std::fs::{create_dir_all, File};
 use std::io::Read;
 use std::path::PathBuf;
@@ -12,12 +13,7 @@ pub(crate) fn cmd_import(args: &ImportArgs) -> Result<()> {
     let out_dir = get_out_path(&mut archive).context("get ROM path")?;
     init_vfs().context("init VFS")?;
     create_dir_all(&out_dir).context("create ROM dir")?;
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).context("find file in archive")?;
-        let out_path = out_dir.join(file.name());
-        let mut outfile = File::create(&out_path).context("create output file")?;
-        std::io::copy(&mut file, &mut outfile).context("copy file from archive")?;
-    }
+    archive.extract(out_dir).context("extract archive")?;
     println!("✅ installed");
     Ok(())
 }
@@ -26,7 +22,10 @@ fn get_out_path(archive: &mut ZipArchive<File>) -> Result<PathBuf> {
     let mut meta_raw = Vec::new();
     let mut meta_file = archive.by_name("meta").context("open meta")?;
     meta_file.read_to_end(&mut meta_raw).context("read meta")?;
-    let meta = firefly_meta::Meta::decode(&meta_raw).context("parse meta")?;
+    if meta_raw.is_empty() {
+        bail!("meta is empty");
+    }
+    let meta = Meta::decode(&meta_raw).context("parse meta")?;
     let vfs_path = get_vfs_path();
     let rom_path = vfs_path.join("roms").join(meta.author_id).join(meta.app_id);
     Ok(rom_path)
