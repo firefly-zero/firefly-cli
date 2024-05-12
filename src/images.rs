ae610f21a4dc7f8548c3a74e7ac3942e0a5775e3
gram <git@orsinium.dev>
2024-05-12 11:04:32 +0200

    support transparency

--- src/images.rs
+++ src/images.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context};
+use anyhow::{bail, Context, Result};
 use image::{Pixel, Rgb, Rgba, RgbaImage};
 use std::fs::File;
 use std::io::Write;
@@ -27,7 +27,7 @@ static DEFAULT_PALETTE: &[Option<Rgb<u8>>] = &[
     Some(Rgb([0x33, 0x3c, 0x57])), // dark gray
 ];
 
-pub fn convert_image(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
+pub fn convert_image(input_path: &Path, output_path: &Path) -> Result<()> {
     let file = image::io::Reader::open(input_path).context("open image file")?;
     let img = file.decode().context("decode image")?;
     let img = img.to_rgba8();
@@ -56,19 +56,24 @@ fn write_image<const BPP: u8, const PPB: usize>(
     mut out: File,
     img: &RgbaImage,
     palette: &[Color],
-) -> anyhow::Result<()> {
+) -> Result<()> {
     write_u8(&mut out, BPP)?; // BPP
     let Ok(width) = u16::try_from(img.width()) else {
         bail!("the image is too big")
     };
     write_u16(&mut out, width)?; // image width
-    write_u8(&mut out, 0xff)?; // transparent color
+    let transparent = pick_transparent(palette)?;
+    write_u8(&mut out, transparent)?; // transparent color
 
     // palette swaps
     let mut byte = 0;
     debug_assert!(palette.len() == 2 || palette.len() == 4 || palette.len() == 16);
     for (i, color) in palette.iter().enumerate() {
-        byte = (byte << 4) | find_color_default(*color);
+        let index = match color {
+            Some(color) => find_color_default(*color),
+            None => transparent,
+        };
+        byte = (byte << 4) | index;
         if i % 2 == 1 {
             write_u8(&mut out, byte)?;
         }
@@ -88,12 +93,12 @@ fn write_image<const BPP: u8, const PPB: usize>(
 }
 
 /// Detect all colors used in the image
-fn make_palette(img: &RgbaImage) -> anyhow::Result<Vec<Color>> {
+fn make_palette(img: &RgbaImage) -> Result<Vec<Color>> {
     let mut palette = Vec::new();
     for pixel in img.pixels() {
         let color = convert_color(*pixel);
         if !palette.contains(&color) {
-            if !DEFAULT_PALETTE.contains(&color) {
+            if color.is_some() && !DEFAULT_PALETTE.contains(&color) {
                 bail!(
                     "found a color not present in the default color palette: {}",
                     format_color(color)
@@ -102,7 +107,10 @@ fn make_palette(img: &RgbaImage) -> anyhow::Result<Vec<Color>> {
             palette.push(color);
         }
     }
-    palette.sort_by_key(|c| find_color(DEFAULT_PALETTE, *c));
+    palette.sort_by_key(|c| match c {
+        Some(c) => find_color_default(*c),
+        None => 20,
+    });
     Ok(palette)
 }
 
@@ -124,8 +132,8 @@ fn write_u16(f: &mut File, v: u16) -> std::io::Result<()> {
 }
 
 /// Find the index of the given color in the default palette.
-fn find_color_default(c: Color) -> u8 {
-    find_color(DEFAULT_PALETTE, c)
+fn find_color_default(c: Rgb<u8>) -> u8 {
+    find_color(DEFAULT_PALETTE, Some(c))
 }
 
 /// Find the index of the given color in the given palette.
@@ -138,6 +146,7 @@ fn find_color(palette: &[Color], c: Color) -> u8 {
     panic!("color not in the default palette")
 }
 
+/// Make human-friendly hex representation of the color code.
 fn format_color(c: Color) -> String {
     match c {
         Some(c) => {
@@ -158,4 +167,21 @@ fn convert_color(c: Rgba<u8>) -> Color {
 const fn is_transparent(c: Rgba<u8>) -> bool {
     let alpha = c.0[3];
     alpha < 128
+}
+
+/// Pick the color to be used to represent transparency
+fn pick_transparent(palette: &[Color]) -> Result<u8> {
+    for (color, i) in DEFAULT_PALETTE.iter().zip(0u8..) {
+        if !palette.contains(color) {
+            return Ok(i);
+        }
+    }
+
+    if palette.len() > 16 {
+        bail!("the image cannot contain more than 16 colors")
+    }
+    if palette.len() == 16 {
+        bail!("an image cannot contain all 16 colors and transparency")
+    }
+    bail!("image contains colors not from the default palette")
 }
