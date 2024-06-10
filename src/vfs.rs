c712f1066f055bc8370b9d077d4c0f52ea85b1c7
gram <git@orsinium.dev>
2024-06-10 15:51:51 +0200

    test name generation

--- src/vfs.rs
+++ src/vfs.rs
@@ -4,7 +4,7 @@ use rand::seq::SliceRandom;
 use rand::{thread_rng, Rng};
 use std::collections::HashMap;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn cmd_vfs() -> anyhow::Result<()> {
@@ -27,7 +27,10 @@ pub fn get_vfs_path() -> PathBuf {
 
 pub fn init_vfs() -> anyhow::Result<()> {
     let path = get_vfs_path();
+    init_vfs_at(&path)
+}
 
+pub fn init_vfs_at(path: &Path) -> anyhow::Result<()> {
     fs::create_dir_all(path.join("roms")).context("create roms directory")?;
     fs::create_dir_all(path.join("sys")).context("create sys directory")?;
     fs::create_dir_all(path.join("data")).context("create data directory")?;
@@ -45,20 +48,24 @@ pub fn init_vfs() -> anyhow::Result<()> {
 
 /// Generate a random device name.
 fn generate_name() -> String {
-    let mut rng = thread_rng();
-
-    let adjs = include_str!("names_adj.txt");
-    let adjs: Vec<_> = adjs.split_whitespace().collect();
-    let adj = adjs.choose(&mut rng).unwrap();
-
-    let nouns = include_str!("names_noun.txt");
-    let nouns: Vec<_> = nouns.split_whitespace().collect();
-    let noun = nouns.choose(&mut rng).unwrap();
-
+    let adj = get_random_line(include_str!("names_adj.txt"));
+    let noun = get_random_line(include_str!("names_noun.txt"));
     let name = format!("{adj}-{noun}");
     leetify(&name)
 }
 
+/// Select a random line from the given text.
+fn get_random_line(adjs: &str) -> &str {
+    let mut rng = thread_rng();
+    debug_assert!(adjs.ends_with('\n'));
+    let adjs = &adjs[..(adjs.len() - 1)];
+    let adjs: Vec<_> = adjs.split_whitespace().collect();
+    adjs.choose(&mut rng).unwrap()
+}
+
+/// Make the given text a bit more 1337-speak.
+///
+/// It replaces with 50% chance every character than can be made leet.
 fn leetify(s: &str) -> String {
     let mut rng = thread_rng();
     let mut replaces = HashMap::new();
@@ -79,4 +86,59 @@ fn leetify(s: &str) -> String {
         res.push(*new);
     }
     res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_init_vfs_at() {
+        let path = std::env::temp_dir().join("test_init_vfs_at");
+        _ = std::fs::remove_dir_all(&path);
+        assert!(!path.exists());
+        init_vfs_at(&path).unwrap();
+        assert_eq!(path.read_dir().unwrap().count(), 3);
+        assert!(path.join("sys").metadata().unwrap().is_dir());
+        assert!(path.join("roms").metadata().unwrap().is_dir());
+        assert!(path.join("data").metadata().unwrap().is_dir());
+        assert_eq!(path.join("roms").read_dir().unwrap().count(), 0);
+        assert_eq!(path.join("data").read_dir().unwrap().count(), 0);
+        assert_eq!(path.join("sys").read_dir().unwrap().count(), 1);
+        let name_path = path.join("sys").join("name");
+        let name = std::fs::read_to_string(name_path).unwrap();
+        assert!(name.contains('-'));
+        assert!(name.len() >= 7);
+        assert!(name.len() <= 15);
+    }
+
+    #[test]
+    fn test_generate_name() {
+        for _ in 0..1000 {
+            let name = generate_name();
+            assert!(name.contains('-'));
+            assert!(name.len() >= 7);
+            assert!(name.len() <= 15);
+            assert!(name.is_ascii());
+            assert!(!name.starts_with('-'));
+            assert!(!name.ends_with('-'));
+            for c in name.chars() {
+                assert!(c.is_ascii_lowercase() || c.is_ascii_digit() || c == '-');
+            }
+        }
+    }
+
+    #[test]
+    fn test_leetify() {
+        for _ in 0..10 {
+            let s = leetify("a");
+            assert!(s == "a" || s == "4");
+            let s = leetify("s");
+            assert!(s == "s" || s == "5");
+            let s = leetify("sg");
+            assert!(s == "sg" || s == "5g");
+            let raw = "the-quick-brown-fox-jumps-over-the-lazy-dog1234567890";
+            assert_eq!(leetify(raw).len(), raw.len());
+        }
+    }
 }
