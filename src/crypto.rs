9d7d6ab80df35354cdd38ecfcaf64688d96f1400
gram <git@orsinium.dev>
2024-06-27 14:33:44 +0200

    test hashing

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,5 @@
 use crate::file_names::{HASH, SIG};
-use anyhow::Context;
+use anyhow::{bail, Context};
 use sha2::digest::consts::U32;
 use sha2::digest::generic_array::GenericArray;
 use sha2::{Digest, Sha256};
@@ -16,6 +16,9 @@ pub fn hash_dir(rom_path: &Path) -> anyhow::Result<GenericArray<u8, U32>> {
     }
     file_paths.sort();
     for path in file_paths {
+        if !path.is_file() {
+            bail!("the ROM dir must contain only files");
+        }
         let file_name = path.file_name().context("get file name")?;
         if file_name == HASH || file_name == SIG {
             continue;
@@ -30,4 +33,28 @@ pub fn hash_dir(rom_path: &Path) -> anyhow::Result<GenericArray<u8, U32>> {
     // write the hash into a file
     let hash = hasher.finalize();
     Ok(hash)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_helpers::*;
+
+    #[test]
+    fn test_hash_dir() {
+        let dir = make_tmp_dir();
+        assert_eq!(dir.read_dir().unwrap().count(), 0);
+        std::fs::write(dir.join("somefile"), "hello").unwrap();
+        let hash1: &[u8] = &hash_dir(&dir).unwrap();
+        let hash2: &[u8] = &hash_dir(&dir).unwrap();
+        assert_eq!(hash1, hash2, "not idempotent");
+
+        std::fs::write(dir.join("somefile"), "hell").unwrap();
+        let hash3: &[u8] = &hash_dir(&dir).unwrap();
+        assert!(hash2 != hash3, "doesn't change if file changed");
+
+        std::fs::write(dir.join("somefile2"), "hell").unwrap();
+        let hash4: &[u8] = &hash_dir(&dir).unwrap();
+        assert!(hash3 != hash4, "doesn't change if fiels added");
+    }
 }
--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -10,3 +10,12 @@ pub fn make_tmp_vfs() -> PathBuf {
     std::fs::create_dir_all(&vfs).unwrap();
     vfs
 }
+
+pub fn make_tmp_dir() -> PathBuf {
+    let mut rng = rand::thread_rng();
+    let n = rng.gen_range(0..100_000);
+    let root = std::env::temp_dir().join(format!("firefly-cli-test-{n}"));
+    _ = std::fs::remove_dir_all(&root);
+    std::fs::create_dir_all(&root).unwrap();
+    root
+}
