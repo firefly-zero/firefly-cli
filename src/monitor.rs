1a026d8710b84649144db1b730dc3c5a62ec6d3d
gram <git@orsinium.dev>
2024-07-26 11:09:37 +0200

    show more cpu stats

--- src/monitor.rs
+++ src/monitor.rs
@@ -52,6 +52,7 @@ fn run_monitor(_args: &MonitorArgs) -> Result<()> {
         let req = serial::Request::Stats(true);
         let buf = req.encode(&mut buf).context("encode request")?;
         stream.write_all(&buf).context("send request")?;
+        stream.flush().context("flush request")?;
     }
 
     let mut stats = Stats {
@@ -73,7 +74,11 @@ fn run_monitor(_args: &MonitorArgs) -> Result<()> {
                 serial::Callback::RenderLine => {}
                 serial::Callback::Cheat => {}
             },
-            serial::Response::CPU(cpu) => stats.cpu = Some(cpu),
+            serial::Response::CPU(cpu) => {
+                if cpu.total_ns > 0 {
+                    stats.cpu = Some(cpu)
+                }
+            }
             serial::Response::Memory(mem) => stats.mem = Some(mem),
         };
         render_stats(&stats)?;
@@ -87,10 +92,39 @@ fn render_stats(stats: &Stats) -> Result<()> {
         execute!(
             io::stdout(),
             cursor::MoveTo(1, 1),
-            style::Print("lag:"),
+            style::Print("lag"),
             cursor::MoveTo(8, 1),
-            style::Print(&format!("{}", cpu.lag_ns)),
+            style::Print(&format_ns(cpu.lag_ns)),
+            cursor::MoveTo(16, 1),
+            style::Print(&format_ratio(cpu.lag_ns, cpu.total_ns)),
+            cursor::MoveTo(1, 2),
+            style::Print("busy"),
+            cursor::MoveTo(8, 2),
+            style::Print(&format_ns(cpu.busy_ns)),
+            cursor::MoveTo(16, 2),
+            style::Print(&format_ratio(cpu.busy_ns, cpu.total_ns)),
+            cursor::MoveTo(1, 3),
+            style::Print("total"),
+            cursor::MoveTo(8, 3),
+            style::Print(&format_ns(cpu.total_ns)),
         )?;
     };
     Ok(())
+}
+
+fn format_ns(ns: u32) -> String {
+    if ns > 1_000_000_000 {
+        return format!("{}s", ns / 1_000_000_000);
+    }
+    if ns > 1_000_000 {
+        return format!("{}ms", ns / 1_000_000);
+    }
+    if ns > 1_000 {
+        return format!("{}μs", ns / 1_000);
+    }
+    format!("{}ns", ns)
+}
+
+fn format_ratio(n: u32, d: u32) -> String {
+    format!("{}%", n as u64 * 100 / d as u64)
 }
