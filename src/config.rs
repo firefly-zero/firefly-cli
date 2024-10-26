244afe00a75873d22c0abb3701f5323e481122a0
gram <git@orsinium.dev>
2024-10-26 18:47:50 +0200

    add boards config

--- src/config.rs
+++ src/config.rs
@@ -37,7 +37,10 @@ pub struct Config {
     pub cheats: Option<HashMap<String, i32>>,
 
     /// Mapping of badge IDs to badges.
-    pub badges: Option<HashMap<u32, BadgeConfig>>,
+    pub badges: Option<HashMap<u16, BadgeConfig>>,
+
+    /// Mapping of board IDs to boards.
+    pub boards: Option<HashMap<u16, BoardConfig>>,
 
     /// Path to the project root.
     #[serde(skip)]
@@ -112,6 +115,39 @@ pub struct BadgeConfig {
     /// If the achievement should be hidden until earned.
     #[serde(default)]
     pub hidden: bool,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct BoardConfig {
+    /// Human-readable board name.
+    pub name: String,
+
+    /// The order in which the board should be displayed, ascending.
+    pub position: Option<u16>,
+
+    /// The minimum value for a score to be added to the board.
+    pub min: Option<u32>,
+
+    /// The maximum value for a score to be added to the board.
+    ///
+    /// Useful for filtering out obvious cheating.
+    pub max: Option<u32>,
+
+    /// If the scores should go in ascending order.
+    ///
+    /// If false (default), uses descending ("larger is better") order.
+    /// Ascending order makes sense for time in racing games.
+    #[serde(default)]
+    pub asc: bool,
+
+    /// If the score should be formatted as time.
+    #[serde(default)]
+    pub time: bool,
+
+    /// Digits after decimal point.
+    #[serde(default)]
+    pub decimals: u8,
 }
 
 #[derive(Deserialize, Debug, Clone)]
