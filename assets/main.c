c9e273689a4e67e09c071699ee9e7a08af7f048f
gram <git@orsinium.dev>
2024-11-26 16:15:08 +0100

    simplify C example

--- assets/main.c
+++ assets/main.c
@@ -13,23 +13,12 @@ UPDATE void update()
 RENDER void render()
 {
     ClearScreen(WHITE);
-
-    Point p1;
-    p1.x = 60;
-    p1.y = 10;
-
-    Point p2;
-    p2.x = 40;
-    p2.y = 40;
-
-    Point p3;
-    p3.x = 80;
-    p3.y = 40;
-
-    Style s;
-    s.fill_color = LIGHT_GRAY;
-    s.stroke_color = DARK_BLUE;
-    s.stroke_width = 1;
-
+    Point p1 = {60, 10};
+    Point p2 = {40, 40};
+    Point p3 = {80, 40};
+    Style s = {
+        .fill_color = LIGHT_GRAY,
+        .stroke_color = DARK_BLUE,
+        .stroke_width = 1};
     DrawTriangle(p1, p2, p3, s);
 }
