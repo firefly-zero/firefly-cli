f7f558ecb363310d8e4b6ba9e80943fe0d82b0da
gram <git@orsinium.dev>
2025-01-01 17:49:30 +0100

    add test helpers for comparing dirs

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -1,5 +1,13 @@
 use rand::Rng;
-use std::path::PathBuf;
+use sha2::{
+    digest::{consts::U32, generic_array::GenericArray},
+    Digest, Sha256,
+};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+};
 
 pub fn make_tmp_vfs() -> PathBuf {
     let mut rng = rand::thread_rng();
@@ -18,4 +26,39 @@ pub fn make_tmp_dir() -> PathBuf {
     _ = std::fs::remove_dir_all(&root);
     std::fs::create_dir_all(&root).unwrap();
     root
+}
+
+/// Recursively compare all files in dirs and ensure they are the same
+pub fn dirs_eq(dir1: &Path, dir2: &Path) {
+    let hashes1 = dir_hashes(dir1);
+    let hashes2 = dir_hashes(dir2);
+    assert_eq!(hashes1.len(), hashes2.len());
+    for (path, hash1) in hashes1 {
+        let hash2 = hashes2[&path];
+        assert_eq!(hash1, hash2);
+    }
+}
+
+/// Get a hashsum for every file in the directory.
+pub fn dir_hashes(dir: &Path) -> HashMap<String, GenericArray<u8, U32>> {
+    let mut results = HashMap::new();
+    let entries = fs::read_dir(dir).unwrap();
+    for entry in entries {
+        let entry = entry.unwrap();
+        let meta = entry.metadata().unwrap();
+        let entry_name = entry.file_name().into_string().unwrap();
+        if meta.is_dir() {
+            let subhashes = dir_hashes(&entry.path());
+            for (file_name, hash) in subhashes {
+                let path = format!("{entry_name}/{file_name}");
+                results.insert(path, hash);
+            }
+        } else {
+            let mut hasher = Sha256::new();
+            let mut file = std::fs::File::open(entry.path()).unwrap();
+            std::io::copy(&mut file, &mut hasher).unwrap();
+            results.insert(entry_name, hasher.finalize());
+        }
+    }
+    results
 }
