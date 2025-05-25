12a46bc3b045f7171ea58eda4cd9c3d9479a971a
gram <git@orsinium.dev>
2025-05-25 12:59:36 +0200

    use color to group logs by time

--- src/commands/logs.rs
+++ src/commands/logs.rs
@@ -11,10 +11,25 @@ pub fn cmd_logs(args: &LogsArgs) -> Result<()> {
         .context("open the serial port")?;
     let mut stream = SerialStream::new(port);
     println!("listening...");
+    let mut prev_log = chrono::Local::now();
+    let mut use_blue = true;
     loop {
         let msg = stream.next()?;
         let now = chrono::Local::now();
-        let now = now.format("%H:%M:%S").to_string().blue();
+        let now_str = now.format("%H:%M:%S").to_string();
+        // When a lot of time has passed since the last log record,
+        // switch the color of the current time.
+        // The color switch makes for an easy visual grouping of log records
+        // coming close to each other in time.
+        if now - prev_log >= chrono::Duration::seconds(4) {
+            use_blue = !use_blue;
+        }
+        prev_log = now;
+        let now = if use_blue {
+            now_str.blue()
+        } else {
+            now_str.magenta()
+        };
         match msg {
             Response::Log(mut log) => {
                 if log.starts_with("ERROR(") {
