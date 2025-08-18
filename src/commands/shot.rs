048d499690360810ee50ea42bf07bd31c6f3adfe
gram <git@orsinium.dev>
2025-08-18 14:17:56 +0200

    correct file size

--- src/commands/shot.rs
+++ src/commands/shot.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 use std::{
     io::Write,
     path::{Path, PathBuf},
@@ -7,20 +7,20 @@ use std::{
 use crate::args::ShotArgs;
 
 const WIDTH: u32 = 240;
-const HEIGHT: u32 = 180;
-const SIZE: usize = 1 + 48 + (180 * 240 / 2);
+const HEIGHT: u32 = 160;
+const SIZE: usize = 1 + 48 + (160 * 240 / 2);
 
 /// Download screenshot.
 pub fn cmd_shot(vfs: &Path, args: &ShotArgs) -> Result<()> {
     let dst_dir: PathBuf = match &args.output {
         Some(dst_dir) => dst_dir.clone(),
-        None => std::env::current_dir()?,
+        None => std::env::current_dir().context("get current dir")?,
     };
     let sources = list_sources(vfs, &args.sources);
     for src_path in sources {
         let dst_file_name = get_output_file_name(&src_path)?;
         let dst_path = dst_dir.join(dst_file_name);
-        download_file(&src_path, &dst_path)?;
+        copy_file(&src_path, &dst_path)?;
     }
     Ok(())
 }
@@ -54,9 +54,9 @@ fn get_output_file_name(src_path: &Path) -> Result<String> {
     let Some((data_idx, _)) = parts.iter().enumerate().find(|(_, part)| **part == "data") else {
         bail!("cannot find data dir")
     };
-    let author_id = parts[data_idx - 2];
-    let app_id = parts[data_idx - 1];
-    let file_name = parts[data_idx + 2];
+    let author_id = parts[data_idx + 1];
+    let app_id = parts[data_idx + 2];
+    let file_name = parts[data_idx + 4];
     let Some((file_id, ext)) = file_name.split_once('.') else {
         bail!("file has no extension");
     };
@@ -66,7 +66,8 @@ fn get_output_file_name(src_path: &Path) -> Result<String> {
     Ok(format!("{author_id}.{app_id}.{file_id}.png"))
 }
 
-fn download_file(src_path: &Path, dst_path: &Path) -> Result<()> {
+/// Read the input screenshot file and save it in output file as PNG.
+fn copy_file(src_path: &Path, dst_path: &Path) -> Result<()> {
     let src_raw = std::fs::read(src_path)?;
     let png_raw = to_png(&src_raw)?;
     std::fs::write(dst_path, png_raw)?;
@@ -81,8 +82,8 @@ fn to_png(raw: &[u8]) -> Result<Vec<u8>> {
     if raw[0] != 0x41 {
         bail!("invalid magic number");
     }
-    let palette: [u8; 48] = raw[1..0x32].try_into().unwrap();
-    let frame = &raw[0x32..];
+    let palette: [u8; 48] = raw[1..0x31].try_into().unwrap();
+    let frame = &raw[0x31..];
 
     let mut w = Vec::new();
     w.write_all(&[137, 80, 78, 71, 13, 10, 26, 10])?;
@@ -112,6 +113,9 @@ fn write_frame<W: Write>(mut w: W, data: &[u8]) -> Result<()> {
 }
 
 /// Each byte in the frame buffer contains 2 pixels. Swap these 2 pixels.
+///
+/// We need to do it because firefly uses little-endian for everything
+/// but PNG is big-endian.
 fn swap_pairs(frame: &[u8]) -> Vec<u8> {
     frame.iter().map(|byte| byte.rotate_left(4)).collect()
 }
@@ -128,4 +132,12 @@ fn write_chunk<W: Write>(mut w: W, name: &[u8; 4], data: &[u8]) -> Result<()> {
     crc.update(data);
     w.write_all(&crc.finalize().to_be_bytes())?;
     Ok(())
+}
+
+/// Convert a file system path to UTF-8 if possible.
+pub fn path_to_utf8(path: &Path) -> anyhow::Result<&str> {
+    match path.to_str() {
+        Some(path) => Ok(path),
+        None => bail!("project root path cannot be converted to UTF-8"),
+    }
 }
