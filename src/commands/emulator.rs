2540299569e29136b69e30a80c36e7699f69d46a
gram <git@orsinium.dev>
2026-01-07 21:47:13 +0100

    drop legacy emulator launch modes

--- src/commands/emulator.rs
+++ src/commands/emulator.rs
@@ -8,76 +8,13 @@ use std::process::Command;
 use tar::Archive;
 
 pub fn cmd_emulator(vfs: &Path, args: &EmulatorArgs) -> Result<()> {
-    let executed_dev = run_dev(args)?;
-    if executed_dev {
-        return Ok(());
-    }
-    let executed_bin = run_bin(args)?;
-    if executed_bin {
-        return Ok(());
-    }
-    run_embedded(vfs, args)
-}
+    #[cfg(target_os = "windows")]
+    const BINARY_NAME: &str = "firefly-emulator.exe";
+    #[cfg(not(target_os = "windows"))]
+    const BINARY_NAME: &str = "firefly-emulator";
 
-fn run_dev(args: &EmulatorArgs) -> Result<bool> {
-    // Check common places where firefly repo might be cloned.
-    // If found, run the dev version using cargo.
-    let Some(home) = std::env::home_dir() else {
-        return Ok(false);
-    };
-    if !binary_exists("cargo") {
-        return Ok(false);
-    }
-    let paths = [
-        home.join("Documents").join("firefly"),
-        home.join("ff").join("firefly"),
-        home.join("github").join("firefly"),
-        home.join("firefly"),
-    ];
-    for dir_path in paths {
-        let cargo_path = dir_path.join("Cargo.toml");
-        if !cargo_path.is_file() {
-            continue;
-        }
-        println!(
-            "⌛ running dev version from {}...",
-            dir_path.to_str().unwrap()
-        );
-        let output = Command::new("cargo")
-            .arg("run")
-            .arg("--")
-            .args(&args.args)
-            .current_dir(dir_path)
-            .output()?;
-        check_output(&output).context("run emulator")?;
-        return Ok(true);
-    }
-    Ok(false)
-}
-
-fn run_bin(args: &EmulatorArgs) -> Result<bool> {
-    let bins = [
-        "./firefly_emulator",
-        "./firefly_emulator.exe",
-        "firefly_emulator",
-        "firefly_emulator.exe",
-        "firefly-emulator",
-        "firefly-emulator.exe",
-    ];
-    for bin in bins {
-        if binary_exists(bin) {
-            println!("⌛ running {bin}...");
-            let output = Command::new(bin).args(&args.args).output()?;
-            check_output(&output).context("run emulator")?;
-            return Ok(true);
-        }
-    }
-    Ok(false)
-}
-
-fn run_embedded(vfs: &Path, args: &EmulatorArgs) -> Result<()> {
     // TODO(@orsinium): always use the global vfs.
-    let bin_path = vfs.join("firefly-emulator");
+    let bin_path = vfs.join(BINARY_NAME);
     if !bin_path.exists() {
         println!("⏳️ downloading emulator...");
         download_emulator(&bin_path).context("download emulator")?;
@@ -118,11 +55,17 @@ fn download_emulator(bin_path: &Path) -> Result<()> {
 }
 
 fn get_release_url() -> Result<String> {
+    #[cfg(target_os = "windows")]
+    const SUFFIX: &str = "x86_64-pc-windows-msvc.tgz";
+    #[cfg(target_os = "macos")]
+    const SUFFIX: &str = "aarch64-apple-darwin.tgz";
+    #[cfg(target_os = "linux")]
+    const SUFFIX: &str = "x86_64-unknown-linux-gnu.tgz";
+
     let version = get_latest_version()?;
-    let suffix = get_suffix();
     let repo = "https://github.com/firefly-zero/firefly-emulator-bin";
     Ok(format!(
-        "{repo}/releases/latest/download/firefly-emulator-v{version}-{suffix}"
+        "{repo}/releases/latest/download/firefly-emulator-v{version}-{SUFFIX}"
     ))
 }
 
@@ -140,21 +83,4 @@ fn get_latest_version() -> Result<String> {
     let loc = loc.to_str()?;
     let version = loc.split('/').next_back().unwrap();
     Ok(version.to_owned())
-}
-
-const fn get_suffix() -> &'static str {
-    #[cfg(target_os = "windows")]
-    return "x86_64-pc-windows-msvc.tgz";
-    #[cfg(target_os = "macos")]
-    return "aarch64-apple-darwin.tgz";
-    #[cfg(target_os = "linux")]
-    return "x86_64-unknown-linux-gnu.tgz";
-}
-
-fn binary_exists(bin: &str) -> bool {
-    let output = Command::new(bin).arg("--help").output();
-    let Ok(output) = output else {
-        return false;
-    };
-    output.status.success()
 }
